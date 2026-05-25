1592316553a090f11d3c8c96f77da7e0511f7df0
Boma <cynthia.ipalibo@gmail.com>
2026-05-25 16:26:05 +1000

    Hide address bar: fullscreen immersive mode + notification permissions

--- android/app/src/main/java/com/fourscans/app/MainActivity.java
+++ android/app/src/main/java/com/fourscans/app/MainActivity.java
@@ -1,5 +1,29 @@
 package com.fourscans.app;
 
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Edge-to-edge: hide system UI bars for a true full-screen native feel
+        getWindow().setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        );
+
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+            | View.SYSTEM_UI_FLAG_FULLSCREEN
+            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        );
+    }
+}
